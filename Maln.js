44cb78cdef244134e4b34634a53633cc720847f1
PrasadGDarpe <103508411+PrasadGDarpe@users.noreply.github.com>
2022-04-12 17:52:55 +0530

    Add files via upload

---
+++ Maln.js
@@ -0,0 +1,37 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+ 
+function draw() {
+    document.getElementById("text").innerHTML = "The Width and Height of The text is = " + difference + "px";
+    background('#969A97');
+    fill('#F90093');
+
+    text("Hello!", noseX, noseY, difference);
+}
+function modelLoaded() {
+    console.log('PoseNet is Intialized');
+}
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX +" rightWristX = "+ rightWristX + "difference = " + difference); 
+    }
+}
